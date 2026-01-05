d9b13426e14ae3a9bbe3ea4bd4d76ee8e7d6f9db
kyzrwolf <kaizerzavr@gmail.com>
2026-01-05 16:35:43 +0300

    lesson_3.1 review fix

--- rangiffler-e2e/src/test/java/io/student/rangiffler/models/UserJson.java
+++ rangiffler-e2e/src/test/java/io/student/rangiffler/models/UserJson.java
@@ -5,16 +5,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.UUID;
 
 public record UserJson(
-    @JsonProperty("id")
     UUID id,
-    @JsonProperty("username")
     String username,
-    @JsonProperty("password")
     String password,
-    @JsonProperty("firstname")
     String firstname,
-    @JsonProperty("surname")
     String surname,
-    @JsonProperty("avatar")
     String avatar
 ) {}
